f0d3c18dd7b44e586625879a858fb781f005629d
Nicole Vega <nicole.vega@epiccode.co>
2026-03-23 21:41:37 -0500

    feat: access modules roles

--- B/src/main.py
+++ B/src/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from src.routers import role_modules
 
 limiter = Limiter(key_func=get_remote_address)
 
@@ -32,3 +33,4 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.include_router(users.router)
 app.include_router(roles.router)
 app.include_router(session.router)
+app.include_router(role_modules.router)
